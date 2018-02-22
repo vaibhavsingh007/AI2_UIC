a64a9c8a82b84416b009f9c55015c2092f128db1
vaibhav singh <vaibhav.singh21@yahoo.co.in>
2018-02-21 23:51:37 -0600

    Implemented world update for stench and bump. (interim commit)

--- src/WorldApplication/AgentFunction.java
+++ src/WorldApplication/AgentFunction.java
@@ -15,7 +15,6 @@
  * 
  */
 
-import java.util.Random;
 import java.util.*;
 
 class AgentFunction {
@@ -33,7 +32,22 @@ class AgentFunction {
     private boolean breeze;
     private boolean stench;
     private boolean scream;
-    private Random rand;
+    //private Random rand;
+    
+	//[][][safe/w/w?, p/p?, bump location]
+	//(0=w,1=w?,3=safe; 0,1=p,p?; 0,1,2,3=BE,BW,BN,BS; -1,1=unexplored,visited)
+	private int[][][] world;
+
+	// Set static world size
+	private int rowSize = 4;
+	private int colSize = 4;
+	private int cellSize = 3;
+	
+	private String agentMove = '';	// R,L,F
+	private String agentOrientation = '';	// E,W,N,S
+	private int agentXLoc;
+	private int agentYLoc;
+
 
     public AgentFunction() {
         // for illustration purposes; you may delete all code
@@ -42,7 +56,11 @@ class AgentFunction {
 
         // this integer array will store the agent actions
         actionTable = new ArrayList();
-
+		world = new int[rowSize][colSize][cellSize];
+		initializeWorld(world);
+		agentOrientation = 'E';		// Agent spawns facing E
+		agentXLoc = rowSize-1;
+		agentYLoc = 0;
         //actionTable.add(0, Action.GO_FORWARD;
         //actionTable.add(1, Action.GO_FORWARD;
         //actionTable.add(2, Action.GO_FORWARD;
@@ -53,8 +71,25 @@ class AgentFunction {
         //actionTable.add(7, Action.SHOOT;
         // new random number generator, for
         // randomly picking actions to execute
-        rand = new Random();
     }
+
+	private void initializeWorld()
+	{
+		for (x=0; x<rowSize; x++)
+		{
+			for (y=0; y<colSize; y++)
+			{
+				world[x][y] = new int[]{-1, -1, -1};
+				//for (z=0; z<cellSize; z++)
+				//{
+				//	world[x][y][z] = -1;
+				//}
+			}
+		}
+
+		// Mark initial location (bottom left)
+		world[rowSize-1][0] = new int[]{2, -1, -1};
+	}
 
     public int process(TransferPercept tp) {
         // To build your own intelligent agent, replace
@@ -84,6 +119,178 @@ class AgentFunction {
         //System.out.println("blahh..");
         return (int) actionTable.get(actionNo);
     }
+    
+    private void updateWorld()
+    {
+		int[] previousAgentLoc = {agentXLoc, agentYLoc};	// Useful if agent executed successful F move.
+		updateAgentLocation();
+
+		// Start with the highest priority updates.
+        if (bump) {
+            // Mark bump location
+			world[agentXLoc][agentYLoc][2] = getBumpPosition();
+        }
+
+        if (stench) {
+            performWumpusUpdate(previousAgentLoc);
+        }
+
+        if (scream) {
+            actionTable.add(0, Action.GO_FORWARD);
+            return;
+        }
+
+        if (breeze) {
+            populateNavigationActions(true);
+//            populateEquiProbNavigationActionsWithShoot(false);
+            return;
+        }
+
+		// If no stench or breeze and move was F, mark adjacent cells safe.
+		if (!stench && !breeze)
+		{
+			markWorldCells(2, 0, getAdjacentCells(), previousAgentLoc);
+		}
+    }
+
+	private void updateAgentLocation()
+	{
+		// Ignore if move not F, or bumped.
+		if (agentMove != 'F' || bump)
+		{
+			return;
+		}
+
+		if (agentOrientation == 'E')
+		{
+			agentYLoc++;
+		}
+		else if (agentOrientation == 'W')
+		{
+			agentYLoc--;
+		}
+		else if (agentOrientation == 'N')
+		{
+			agentXLoc--;
+		}
+		else if (agentOrientation == 'S')
+		{
+			agentXLoc++;
+		}
+	}
+
+	private void performWumpusUpdate(int[] previousAgentLoc)
+	{
+		// Update adjacent w? to w
+		ArrayList<int[]> adjacentCells = getAdjacentCells();
+		wumpusConfirmed = false;
+
+		for (int i=0; i<adjacentCells.size(); i++)
+		{
+			int[] cell = adjacentCells.get(i);
+
+			if (world[cell[0]][cell[1]][0] == 1)	// w?
+			{
+				world[cell[0]][cell[1]][0] = 0;		// w
+				wumpusConfirmed = true;
+			}
+		}
+
+		if (!wumpusConfirmed)
+		{
+			// Mark all adjacent as w?
+			markWorldCells(1, 0, adjacentCells, previousAgentLoc);
+		}
+		else
+		{
+			// Once wumpus is confirmed, remove all other w?. And if no p?/p, mark safe.
+			removeWAndMarkSafe();
+		}
+	}
+
+	// Only deals with marking w/p/w?/p?/safe
+	private void markWorldCells(int marker, int markerIndex, ArrayList<int[]> cells, int[] previousAgentLoc)
+	{
+		// Proceed only if F and no bump
+		if (agentMove != 'F' || bump)
+		{
+			return;
+		}
+
+		for (int i=0; i<cells.size(); i++)
+		{
+			currentCell = cells.get(i);
+
+			// Exclude the cell agent came from, or safe.
+			if (Arrays.equals(currentCell, previousAgentLoc) || world[currentCell[0]][currentCell[1]][0] == 2)
+			{
+				continue;
+			}
+
+			world[currentCell[0]][currentCell[1]][markerIndex] = marker;
+		}
+	}
+
+	// Remove w? and marks world cells safe.
+	private void removeWAndMarkSafe()
+	{
+		for (x=0; x<rowSize; x++)
+		{
+			for (y=0; y<colSize; y++)
+			{
+				if (world[x][y][0] == 1 && world[x][y][1] == -1)	// w? and not p?/p
+				{
+					world[x][y][0] = 2;
+				}
+			}
+		}
+	}
+
+	private ArrayList<int[]> getAdjacentCells()
+	{
+		ArrayList<int[]> retval = new ArrayList<int[]>();
+
+		// Check bounds
+		if (agentYLoc > 0)
+		{
+			retval.add(new int[]{agentXLoc, agentYLoc-1});
+		}
+		if (agentYLoc < colSize -1)
+		{
+			retval.add(new int[]{agentXLoc, agentYLoc+1});
+		}
+		if (agentXLoc > 0)
+		{
+			retval.add(new int[]{agentXLoc-1, agentYLoc});
+		}
+		if (agentXLoc < rowSize-1)
+		{
+			retval.add(new int[]{agentXLoc+1, agentYLoc});
+		}
+
+		return retval;
+	}
+
+	private int getBumpPosition()
+	{
+		if (agentOrientation == 'E')
+		{
+			return 0;
+		}
+		if (agentOrientation == 'W')
+		{
+			return 1;
+		}
+		if (agentOrientation == 'N')
+		{
+			return 2;
+		}
+		if (agentOrientation == 'S')
+		{
+			return 3;
+		}
+		return -1;
+	}
 
     private void populateValidActions() {
         // Start with the highest priority actions
@@ -116,31 +323,6 @@ class AgentFunction {
         populateNavigationActions(false);
     }
 
-    // Helper method to populate normal navigation actions with P(F) = 67%
-    private void populateNavigationActions(boolean onlySideways) {
-        actionTable.add(0, Action.TURN_RIGHT);
-        actionTable.add(1, Action.TURN_LEFT);
-
-        if (!onlySideways) {
-            actionTable.add(2, Action.GO_FORWARD);
-            actionTable.add(3, Action.GO_FORWARD);
-            actionTable.add(4, Action.GO_FORWARD);
-            actionTable.add(5, Action.GO_FORWARD);
-        }
-    }
-
-    // Populates all navigation actions and shoot with equal probabilities.
-    private void populateEquiProbNavigationActionsWithShoot() {
-        actionTable.add(0, Action.GO_FORWARD);
-        actionTable.add(1, Action.TURN_RIGHT);
-        actionTable.add(2, Action.TURN_LEFT);
-
-        // Appreciate probability of SHOOT to 57%
-        actionTable.add(3, Action.SHOOT);
-        actionTable.add(4, Action.SHOOT);
-        actionTable.add(5, Action.SHOOT);
-        actionTable.add(6, Action.SHOOT);
-    }
 
     // public method to return the agent's name
     // do not remove this method
